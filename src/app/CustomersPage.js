b78c7d4e1904dabdd0573963aad75d5e4f2fa999
Carlos Eduardo Díaz Valbuena <carlos@zuluapp.io>
2024-01-27 18:33:45 -0500

    changes

--- src/app/CustomersPage.js
+++
@@ -1,5 +0,0 @@
-export const CustomersPage = () => {
-    return (
-        <>Customers</>
-    );
-};
--- src/helpers/formatName.js
+++
@@ -1,3 +0,0 @@
-export const formatName = ({ firstName, middleName, lastName, secondLastName, nickname }) => {
-    return `${firstName||''} ${middleName||''} ${lastName||''} ${secondLastName||''} ${ nickname === firstName || nickname === middleName ? '' : `(${nickname})` }`;
-};
